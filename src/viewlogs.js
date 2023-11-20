7ce422b8a24dffdb5af9b68671333b0dadb97951
ash366 <114457962+ash366@users.noreply.github.com>
2023-11-19 18:52:30 -0800

    Create viewlogs.js

---
+++ src/viewlogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ViewLog = () => {
+  return (
+    <div>
+      <h2>View Log Page</h2>
+      <p>Hello, World!</p>
+    </div>
+  );
+};
+
+export default ViewLog;
