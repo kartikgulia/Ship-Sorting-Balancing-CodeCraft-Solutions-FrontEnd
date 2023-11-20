721d2a797a77b7d9649772e83eb2b60b7b395d7f
RayyanZaid <rayyanzaid0401@gmail.com>
2023-11-20 02:37:25 -0800

    UNUSED and NAN done

--- src/components/SelectContainerComponents/ContainerComponent.jsx
+++ src/components/SelectContainerComponents/ContainerComponent.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 const ContainerComponent = ({ value, isSelected, onClick }) => {
-  const style = isSelected ? selectedStyle : cellStyle;
+  let style;
+  let displayContent;
+
+  if (value === "NAN") {
+    // Use a special style for "NAN" to fill the whole container
+    style = { ...cellStyle, padding: 0 };
+    displayContent = <div style={nanStyle}></div>;
+  } else if (value !== "UNUSED") {
+    // Regular display for other values
+    style = isSelected ? selectedStyle : cellStyle;
+    displayContent = value;
+  } else {
+    // Apply the regular style when value is "UNUSED"
+    style = isSelected ? selectedStyle : cellStyle;
+  }
 
   return (
     <div style={style} onClick={onClick}>
-      {value}
+      {displayContent}
     </div>
   );
 };
@@ -20,6 +34,12 @@ const cellStyle = {
 const selectedStyle = {
   ...cellStyle,
   backgroundColor: "green",
+};
+
+const nanStyle = {
+  width: "100%",
+  height: "100%",
+  backgroundColor: "black",
 };
 
 export default ContainerComponent;
