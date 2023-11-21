251fd5f1046beb189a339f8b0cec68435dc3f915
kartikgulia <kartik.gulia03@gmail.com>
2023-11-20 16:18:43 -0800

    Added report issue input box with submit button and word count. Still need to connect submit to backend

--- src/report-issue.js
+++ src/report-issue.js
@@ -1,12 +1,50 @@
 import React, { useState } from 'react';
 
 
-function Issue() {
+// function Issue() {
+//   return (
+//     <div>
+//       <h1>Report Issue Page</h1>
+//     </div>
+//   );
+// };
+
+// export default Issue;
+
+const WordCounter = () => {
+  const [paragraph, setParagraph] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+
+  const handleInputChange = (event) => {
+    const inputText = event.target.value;
+    setParagraph(inputText);
+
+    // Calculate word count
+    const words = inputText.split(/\s+/);
+    setWordCount(words.length);
+  };
+
+  const handleSubmit = () => {
+    // Add your submission logic here
+    console.log('Submitted paragraph:', paragraph);
+  };
+
   return (
     <div>
-      <h1>Report Issue Page</h1>
+      <div>
+       <h1>Report Issue</h1>
+      </div>
+      <textarea
+        placeholder="Enter Text"
+        value={paragraph}
+        onChange={handleInputChange}
+      />
+      <div style={{ textAlign: 'right' }}>
+        Word Count: {wordCount}
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
 
-export default Issue;
+export default WordCounter;
