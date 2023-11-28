c44869ecb5fa6aff1444018f9418f727a3c21cc1
kartikgulia <kartik.gulia03@gmail.com>
2023-11-28 11:51:50 -0800

    Added YT video to tutorial screen, you'll need to do npm install react-youtube in order for it to work

--- src/screens/tutorial.js
+++ src/screens/tutorial.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import YouTube from "react-youtube";
 
-const Tutorial = () => {
-  return (
-    <div>
-      <h2>Tutorial Page</h2>
-      <p>Hello, World!</p>
-    </div>
-  );
-};
 
-export default Tutorial;
+ class MovieClip extends React.Component {
+    render() {
+      const options = {
+        height: '490',
+        width: '640',
+        playerVars: {
+          autoplay: 1,
+          controls: 1,
+        },
+      };
+  
+      return <YouTube videoId="dQw4w9WgXcQ" options={options} onReady={this._onReady} id="video"/>;
+    }
+  
+    _onReady(event) {
+      event.target.pauseVideo();
+    }
+  }
+
+  export default MovieClip;
+// const Tutorial = () => {
+//   return (
+//     <div>
+//       <h2>Tutorial Page</h2>
+//       <p>Hello, World!</p>
+//     </div>
+//   );
+// };
+
+// export default Tutorial;
