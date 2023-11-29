b36013622f5ab87fbd14e3e4f3cba28dc2b4ceb5
RayyanZaid <rayyanzaid0401@gmail.com>
2023-11-29 01:39:54 -0800

    added done button

--- src/screens/OperationListScreen.js
+++ src/screens/OperationListScreen.js
@@ -4,6 +4,7 @@ import GridComp from "../components/OperationAnimationComponents/GridComponent";
 function OperationListScreen({ isBalance }) {
   const [moves, setMoves] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isDone, setIsDone] = useState(false); // State to track if the final move is reached
 
   const handleButtonClick = () => {
     fetch("http://127.0.0.1:5000/balance")
@@ -17,6 +18,7 @@ function OperationListScreen({ isBalance }) {
         console.log("Balance data:", data);
         setMoves(data.listOfMoves); // Assuming data.listOfOperations is the array of moves
         setCurrentIndex(0); // Reset to the first move
+        setIsDone(false); // Reset the "done" state when new moves are loaded
       })
       .catch((error) => {
         console.error("There was a problem with the fetch operation:", error);
@@ -25,10 +27,21 @@ function OperationListScreen({ isBalance }) {
 
   const goToNextMove = () => {
     setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, moves.length - 1));
+    // Check if the next move is the last move to enable the "done" button
+    if (currentIndex === moves.length - 2) {
+      setIsDone(true);
+    }
   };
 
   const goToPreviousMove = () => {
     setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+    setIsDone(false); // Disable "done" button when navigating to a previous move
+  };
+
+  const handleDoneClick = () => {
+    // Handle what happens when the user clicks the "done" button for the final move
+    // You can add your logic here.
+    console.log("User clicked 'Done' for the final move.");
   };
 
   const currentMove = moves.length > 0 ? moves[currentIndex] : null;
@@ -44,7 +57,6 @@ function OperationListScreen({ isBalance }) {
             <div>
               Move {currentIndex + 1} of {moves.length}
             </div>
-            <div>{JSON.stringify(moves[currentIndex])}</div>
             <button onClick={goToPreviousMove} disabled={currentIndex === 0}>
               Previous
             </button>
@@ -54,6 +66,7 @@ function OperationListScreen({ isBalance }) {
             >
               Next
             </button>
+            {isDone && <button onClick={handleDoneClick}>Done</button>}
           </>
         )}
       </div>
