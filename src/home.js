35980bbce14a2928944f5a7dc5aaa68ebdc8db9a
ash366 <114457962+ash366@users.noreply.github.com>
2023-12-03 14:25:35 -0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,24 +1,46 @@
+import React, { useState } from 'react';
+
+import UploadScreen from './UploadManifest'; // Import the UploadScreen component
+
 const Home = () => {
+  const [showUploadScreen, setShowUploadScreen] = useState(false);
+
   const handleTransfer = () => {
-    // Add functionality for the Transfer button
+    // Add logic for transferring
     console.log('Transfer button clicked');
-    // Add logic for transfer functionality if required
+    // Example: Trigger a transfer function or API call
+
+    // Show the UploadScreen component
+    setShowUploadScreen(true);
   };
 
   const handleCheckBalance = () => {
-    // Add functionality for the Balance button
+    // Add logic for checking balance
     console.log('Balance button clicked');
-    // Add logic for checking balance if required
+    // Example: Fetch balance information
+
+    // Show the UploadScreen component
+    setShowUploadScreen(true);
   };
 
   return (
     <div className="home-container">
-      <h2>Home</h2>
-      <div className="button-row">
-        <button className="action-button transfer-button" onClick={handleTransfer}>Transfer</button>
-        <button className="action-button balance-button" onClick={handleCheckBalance}>Balance</button>
-      </div>
-      {/* Additional content for the Home component if needed */}
+      {showUploadScreen ? (
+        <UploadScreen />
+      ) : (
+        <div>
+          <h2>Home</h2>
+          <div className="button-row">
+            <button className="action-button transfer-button" onClick={handleTransfer}>
+              Transfer
+            </button>
+            <button className="action-button balance-button" onClick={handleCheckBalance}>
+              Balance
+            </button>
+          </div>
+          {/* Additional content for the Home component if needed */}
+        </div>
+      )}
     </div>
   );
 };
