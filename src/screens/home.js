c41b03a4c07e1638b3e6c0a6eafff3c9467c8856
ash366 <114457962+ash366@users.noreply.github.com>
2023-12-13 13:35:19 -0800

    Update home.js

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,32 +1,33 @@
 import React, { useState } from "react";
 
 import UploadScreen from "./UploadManifest"; // Import the UploadScreen component
-
+import "./home.css"
 const Home = () => {
   const [showUploadScreen, setShowUploadScreen] = useState(false);
-  const [isBalance, setIsBalance] = useState(false);
 
   const handleTransfer = () => {
-    // Logic for transferring
+    // Add logic for transferring
     console.log("Transfer button clicked");
-    setIsBalance(false); // Set isBalance to false
-    setShowUploadScreen(true); // Show the UploadScreen component
+    // Example: Trigger a transfer function or API call
+
+    // Show the UploadScreen component
+    setShowUploadScreen(true);
   };
 
   const handleCheckBalance = () => {
-    // Logic for checking balance
+    // Add logic for checking balance
     console.log("Balance button clicked");
-    setIsBalance(true); // Set isBalance to true
-    setShowUploadScreen(true); // Show the UploadScreen component
+    // Example: Fetch balance information
+
+    // Show the UploadScreen component
+    setShowUploadScreen(true);
   };
 
   return (
     <div className="home-container">
       {showUploadScreen ? (
-        // Render UploadScreen with isBalance based on the state
-        <UploadScreen isBalance={isBalance ? 1 : 0} />
+        <UploadScreen />
       ) : (
-        // Render buttons when showUploadScreen is false
         <div>
           <h2>Home</h2>
           <div className="button-row">
