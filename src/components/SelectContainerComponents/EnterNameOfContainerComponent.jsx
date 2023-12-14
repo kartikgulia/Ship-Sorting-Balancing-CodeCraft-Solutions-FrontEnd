e9695838d0db5a00f804c328b1a68a231a572528
RayyanZaid <rayyanzaid0401@gmail.com>
2023-12-14 01:52:21 -0800

    accounted for description prohibitions

--- src/components/SelectContainerComponents/EnterNameOfContainerComponent.jsx
+++ src/components/SelectContainerComponents/EnterNameOfContainerComponent.jsx
@@ -1,31 +1,51 @@
 import React, { useState } from "react";
-import './EnterNameOfContainerComponents.css'; 
+import "./EnterNameOfContainerComponents.css";
 
 export default function EnterNameOfContainerComponent({ onNamesUpdate }) {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [names, setNames] = useState([]);
+  const [inputName, setInputName] = useState(""); // State for the current input name
+  const [error, setError] = useState(""); // State to hold error message
 
   const handleNext = () => {
-    setCurrentSlide(currentSlide + 1);
-    onNamesUpdate(names);
+    if (inputName.length > 256) {
+      setError(
+        `Description cannot be more than 256 characters. You typed ${inputName.length} characters`
+      );
+    } else if (inputName === "NAN" || inputName === "UNUSED") {
+      setError('Name cannot be "NAN" or "UNUSED"');
+    } else {
+      const newNames = [...names];
+      newNames[currentSlide] = inputName;
+      setNames(newNames);
+      onNamesUpdate(newNames);
+
+      setCurrentSlide(currentSlide + 1);
+      setInputName(""); // Reset input field
+      setError(""); // Reset error message
+    }
   };
 
   const handleInputChange = (e) => {
-    const newNames = [...names];
-    newNames[currentSlide] = e.target.value;
-    setNames(newNames);
+    setInputName(e.target.value);
   };
 
   return (
-    <div className="container-style"> 
+    <div className="container-style">
       <h1> Enter name </h1>
       <input
         type="text"
-        value={names[currentSlide] || ""}
+        value={inputName}
         onChange={handleInputChange}
-        className="input-style" 
+        // onPaste={(e) => e.preventDefault()}
+        // onCopy={(e) => e.preventDefault()}
+        className="input-style"
       />
-      <button onClick={handleNext} className="button-style">Confirm Name</button> 
+
+      <button onClick={handleNext} className="button-style">
+        Confirm Name
+      </button>
+      {error && <p className="error-message">{error}</p>}
       <div>
         <h2>Entered Names:</h2>
         {names.map((name, index) => (
